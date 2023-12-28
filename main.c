283f9a2f0cbd61ae24d2d628590e33571079275e
Carlos Martínez <ced.mendoza@gmail.com>
2023-12-28 10:21:16 -0600

    vendor: Add vendoring and raylib dependency

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include <raylib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    InitWindow(800, 800, "SimpleFlowField");
+    SetWindowMonitor(0);
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        {
+            ClearBackground(BLACK);
+            DrawText("Hello world!", 0, 30, 18, WHITE);
+            DrawFPS(0, 0);
+        }
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
